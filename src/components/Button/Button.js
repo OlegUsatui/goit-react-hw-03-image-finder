3fddc7361a800385af070d69ebfe34178d8739e1
Oleg Usatyi <olegysik1@ukr.net>
2021-09-28 16:48:09 +0300

    refactoring button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,7 @@
 import css from "./Button.module.css";
 
-const Button = ({text, onLoadMoreClick}) => {
+export default function Button ({text, onLoadMoreClick}) {
     return (
         <button type="button" className={css.Button} onClick={onLoadMoreClick}>{text}</button>
     )
 };
-
-export default Button;
